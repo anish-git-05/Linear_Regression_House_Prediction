3bdc95e015cc497b3381d1fb0012462491e64b2f
Anish Kumar <anish582006@gmail.com>
2026-03-11 22:38:44 +0530

    Fixed file name of bias.npy

--- server/Model.py
+++ server/Model.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.dirname(__file__)
 
 weights = np.load(os.path.join(BASE_DIR, "w.npy"))
-bias = np.load(os.path.join(BASE_DIR, "bias.npy"))
+bias = np.load(os.path.join(BASE_DIR, "b.npy"))
 mean = np.load(os.path.join(BASE_DIR, "mean.npy"))
 std = np.load(os.path.join(BASE_DIR, "std.npy"))
 def predict(features):
